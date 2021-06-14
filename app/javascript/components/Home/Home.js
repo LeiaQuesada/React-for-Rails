1ce5d066a8413b3abd52167fd63def63728245ac
Leia Quesada <leiaquesada@gmail.com>
2021-06-14 12:25:02 -0700

    Render React Home component

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,5 +1,42 @@
--Jumbotron--TableOfContents/List
- -Items
-  -Preview/Thumbnail
- -ActiveItem
-  -Video
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course_modules: [
+        {
+          id: 1,
+          title: "Setting up a new Ruby on Rails App with React",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 2,
+          title: "Adding React to an Existing Rails App",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 3,
+          title: "Building a Hello World App",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 4,
+          title: "Adding React Router Dom to your App",
+          description: "lorem ipsum",
+          active: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return <div>This is our Home component</div>;
+  }
+}
+
+export default Home;
