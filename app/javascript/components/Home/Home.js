4737162979650ba7abc072ccc1bffb6d7fbe1e21
Leia Quesada <leiaquesada@gmail.com>
2021-06-14 12:43:07 -0700

    Render Jumbotron of sample course video

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import Jumbotron from "./Jumbotron";
 
 class Home extends Component {
   constructor() {
@@ -35,7 +35,11 @@ class Home extends Component {
   }
 
   render() {
-    return <div>This is our Home component</div>;
+    return (
+      <div>
+        <Jumbotron />
+      </div>
+    );
   }
 }
 
